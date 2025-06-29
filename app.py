aa79ad9172b3a44fa4bb95afc74c14572cc875b6
Darius Pirnia <dariuspirnia@gmail.com>
2025-06-29 08:49:30 -0400

    integrated the web app with the orriginal link app

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+app.secret_key = "0123456789876543210"
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html', page_title='Dollar By Dollar - About Us')
+
+@app.route('/resources')
+def resources_page():
+    return render_template('resources.html')
+
+@app.route('/team')
+def team_page():
+    return render_template('team.html', page_title='Dollar By Dollar - Our Team')
 
 
